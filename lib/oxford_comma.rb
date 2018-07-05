113f5230d4e2218fc0d95e3ea11906b81ee11980
amiratak88@gmail.com <amiratak88@gmail.com>
2018-07-05 23:20:34 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,7 @@ def oxford_comma(array)
       array.join( and )
     when 3
       "#{array[0]}, #{array[1]}, and #{array[2]}"
-    when > 3
+    else
+      arr2 = array.pop.join(", ") + ", and #{array[length - 1]}"
   end
 end
