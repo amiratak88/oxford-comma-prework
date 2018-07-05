0c5de9b5b0c811e3ba72453f0d98e4a5f32b947c
amiratak88@gmail.com <amiratak88@gmail.com>
2018-07-05 23:13:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,9 @@ def oxford_comma(array)
   length = array.length
   case length
     when 2
-      
+      array.join( and )
+    when 3
+      "#{array[0]}, #{array[1]}, and #{array[2]}"
+    when > 3
   end
 end
