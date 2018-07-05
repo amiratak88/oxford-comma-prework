d1824865ba4e805cb6c2fedebdf7ee60a1498260
amiratak88@gmail.com <amiratak88@gmail.com>
2018-07-05 23:26:56 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,8 +6,8 @@ def oxford_comma(array)
     when 1
       "#{array[0]}"
     else
-      arr2 = array
-      arr2.pop
-      arr2.join(", ") + ", and #{array[-1]}"
+      last = array[-1]
+      array.pop
+      array.join(", ") + ", and #{last}"
   end
 end
