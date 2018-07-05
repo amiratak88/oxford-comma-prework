6ae0692dae82b00d6692b3a54454e2c6166a11f4
amiratak88@gmail.com <amiratak88@gmail.com>
2018-07-05 23:25:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,8 @@ def oxford_comma(array)
     when 1
       "#{array[0]}"
     else
-      arr2 = array.pop
+      arr2 = array
+      arr2.pop
+      arr2.join(", ") + ", and #{array[-1]}"
   end
 end
